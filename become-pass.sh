073e6dc5d984d6f9e07ecb429056f7f6bfb7a2f6
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-07-06 13:12:28 -0700

    fix: perms

--- become-pass.sh
+++ become-pass.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -e
 env BW_SESSION=`cat .bw_session` $HOME/.nvm/versions/node/v24.3.0/bin/node $HOME/.nvm/versions/node/v24.3.0/lib/node_modules/@bitwarden/cli/build/bw.js get password 'fabricant admin'
+ 
